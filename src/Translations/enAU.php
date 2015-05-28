c72ff8be17453eb22d61e261d516508125aff0eb
Jack Polgar <jack@polgar.id.au>
2015-05-28 19:46:49 +1000

    Add translations for forms, confirmations and pagination.

--- src/Translations/enAU.php
+++ src/Translations/enAU.php
@@ -24,18 +24,21 @@ use Avalon\Language;
  * The English translations for Avalon.
  * @package Avalon\Language\Translations
  */
-$enAU = new Language(function($t) {
+$enAU = new Language(function ($t) {
     $t->name    = "English (Australian)";
     $t->locale  = "enAU";
     $t->strings = array(
+        // ---------------------------------------------------------------------
         // Model validations
-        'errors.validations.already_in_use'  => "{field} is already in use",
-        'errors.validations.required'        => "{field} is required",
-        'errors.validations.must_be_email'   => "{field} is not a valid email",
-        'errors.validations.field_too_short' => "{field} must be at least {minLength} characters long",
-        'errors.validations.field_too_long'  => "{field} must be under {maxLength} characters long",
-        'errors.validations.must_be_numeric' => "{field} must be a number",
+        'errors.validations.already_in_use'    => "{field} is already in use",
+        'errors.validations.required'          => "{field} is required",
+        'errors.validations.must_be_email'     => "{field} is not a valid email",
+        'errors.validations.field_too_short'   => "{field} must be at least {minLength} characters long",
+        'errors.validations.field_too_long'    => "{field} must be under {maxLength} characters long",
+        'errors.validations.must_be_numeric'   => "{field} must be a number",
+        'errors.validations.fields_dont_match' => "{field}'s don't match",
 
+        // ---------------------------------------------------------------------
         // Time helper
         'time.x_second' => "{plural:{1}, {{1} second|{1} seconds}}",
         'time.x_minute' => "{plural:{1}, {{1} minute|{1} minutes}}",
@@ -45,6 +48,28 @@ $enAU = new Language(function($t) {
         'time.x_month'  => "{plural:{1}, {{1} month|{1} months}}",
         'time.x_year'   => "{plural:{1}, {{1} year|{1} years}}",
         'time.x_and_x'  => "{1} and {2}",
-        'time.x_ago'    => "{1} ago"
+        'time.x_ago'    => "{1} ago",
+
+        // ---------------------------------------------------------------------
+        // Forms
+        'submit'           => "Submit",
+        'save'             => "Save",
+        'create'           => "Create",
+        'cancel'           => "Cancel",
+        'close'            => "Close",
+        'edit'             => "Edit",
+        'delete'           => "Delete",
+        'confirm_password' => "Confirm Password",
+
+        // ---------------------------------------------------------------------
+        // Confirmations:
+        'confirm.yes'    => "Yes",
+        'confirm.no'     => "No",
+        'confirm.delete' => "Are you sure?",
+
+        // ---------------------------------------------------------------------
+        // Pagination
+        'next'     => "Next",
+        'previous' => "Previous"
     );
 });
