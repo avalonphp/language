21ebcbbd23445b881cc2b066627f9f5064159b30
Jack Polgar <jack@polgar.id.au>
2016-01-30 20:11:42 +1100

    Update copyright year.

--- src/Language.php
+++ src/Language.php
@@ -1,7 +1,7 @@
 <?php
 /*!
  * Avalon
- * Copyright 2011-2015 Jack P.
+ * Copyright 2011-2016 Jack P.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
